62915fc35c66fbfcb6e9be0f026089453daa71cc
Rohit Bhatt <rohitbhatt@q2pay.in>
2025-05-24 13:30:38 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import "./index.css"
+import React from "react";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./container/Layout";
+import Appointments from "./container/Appointments";
+import Calendar from "./container/Calendar";
+import Chats from "./container/Chats";
+import Dashboard from "./container/Dashboard";
+import History from "./container/History";
+import Settings from "./container/Settings";
+import Supports from "./container/Supports";
+import Statistics from "./container/Statistics";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="appointments" element={<Appointments />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="history" element={<History />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="supports" element={<Supports />} />
+          <Route path="statistics" element={<Statistics />} />
+          <Route path="chats" element={<Chats />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/container/History/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const History = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default History
