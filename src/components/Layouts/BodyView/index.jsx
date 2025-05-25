c189e68c49048184c8fefffadf195798e161d2c8
Rohit Bhatt <rohitbhatt@q2pay.in>
2025-05-25 13:33:26 +0530

    done

--- src/components/Layouts/BodyView/index.jsx
+++ src/components/Layouts/BodyView/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./index.css";
 import { FaChevronDown } from "react-icons/fa";
-import body from "../../../assets/—Pngtree—3d anatomical model of a_20980986.png"
-import lungs from "../../../assets/—Pngtree—red lung pink lung tube_3874739.png"
-import teeth from "../../../assets/—Pngtree—close-up of a perfectly white_20487178.png"
-import bone from "../../../assets/—Pngtree—toon bone_8357847.png"
+import body from "../../../assets/—Pngtree—3d anatomical model of a_20980986.png";
+import lungs from "../../../assets/—Pngtree—red lung pink lung tube_3874739.png";
+import teeth from "../../../assets/—Pngtree—close-up of a perfectly white_20487178.png";
+import bone from "../../../assets/—Pngtree—toon bone_8357847.png";
 
 import { FaSearchPlus } from "react-icons/fa";
 
@@ -20,24 +20,41 @@ const BodyView = () => {
       </div>
       <div className="right-body-bottom">
         <div className="right-body-bottom-left">
-            <FaSearchPlus className="plus-search"/>
-            <img src={body} alt="body"/>
+          <FaSearchPlus className="plus-search" />
+          <img src={body} alt="body" />
         </div>
         <div className="right-body-bottom-right">
-            <div className="right-body-bottom-right-1">
-                <div className="box-image-div">
-                    <img src={lungs} alt="lungs"/>
-                    <p>Lungs</p>
-                </div>
-                <p>Date : 26 Oct 2021</p>
-                <div className="box-line"></div>
-
+          <div className="right-body-bottom-right-1">
+            <div className="box-image-div">
+              <img src={lungs} alt="lungs" />
+              <p>Lungs</p>
             </div>
-            <div className="right-body-bottom-right-1"></div>
-            <div className="right-body-bottom-right-1"></div>
-
+            <p>Date : 26 Oct 2021</p>
+            <div class="progress-bar">
+              <div class="progress-fill" style={{backgroundColor:"#ff4d4f"}}></div>
+            </div>
+          </div>
+          <div className="right-body-bottom-right-1">
+            <div className="box-image-div">
+              <img src={teeth} alt="teeth" />
+              <p>Teeth</p>
+            </div>
+            <p>Date : 26 Oct 2021</p>
+            <div class="progress-bar">
+              <div class="progress-fill" style={{backgroundColor:"#36d399"}}></div>
+            </div>
+          </div>
+           <div className="right-body-bottom-right-1">
+            <div className="box-image-div">
+              <img src={bone} alt="bone" />
+              <p>Bone</p>
+            </div>
+            <p>Date : 26 Oct 2021</p>
+            <div class="progress-bar">
+              <div class="progress-fill" style={{backgroundColor:"#f97316"}}></div>
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   );
